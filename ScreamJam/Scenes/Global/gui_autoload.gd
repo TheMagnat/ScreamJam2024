8cac39cffeebb4f32a4687c856e5fda371728d75
buloz <lbizeray30@gmail.com>
2024-10-17 02:18:26 +0200

    [ui] pause menu w/ controls

---
+++ ScreamJam/Scenes/Global/gui_autoload.gd
@@ -0,0 +1,32 @@
+extends CanvasLayer
+
+var pauseMenuPath = "res://Scenes/Menu/PauseMenu.tscn"
+
+var resolutions = {
+	"2560x1440": Vector2i(2560,1440),
+	"1920x1080": Vector2i(1920,1080),
+	"1366x768": Vector2i(1366,768),
+	"1280x720": Vector2i(1280,720),
+	"1920x1200" : Vector2i(1920,1200),
+	"1680x1050" : Vector2i(1680,1050),
+	"1440x900" : Vector2i(1440,900),
+	"1280x800" : Vector2i(1280,800),
+	"1024x768" : Vector2i(1024,768),
+	"800x600" : Vector2i(800,600),
+	"640x480" : Vector2i(640,480),
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var pauseMenuScene = load(pauseMenuPath).instantiate()
+	add_child(pauseMenuScene)
+	pauseMenuScene.hide()
+	pass # Replace with function body.
+
+
+func _input(_event):
+	if Input.is_action_just_pressed("Pause"):
+		var pauseMenu = get_node("PauseMenu")
+		pauseMenu.visible = !pauseMenu.visible
+		if pauseMenu.visible:
+			pauseMenu.update_values()
---
+++ ScreamJam/Scenes/Menu/pause_menu.gd
@@ -0,0 +1,37 @@
+extends Control
+
+@onready var resolutionOptions = $Panel/GridContainer/ResolutionOptions
+
+@onready var master_bus = AudioServer.get_bus_index("Master")
+
+const VOLUME_LIMIT = -40
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	add_resolutions()
+
+func add_resolutions():
+	for i in GuiAutoload.resolutions:
+		resolutionOptions.add_item(i)
+
+func update_values():
+	var windows_size = str(get_window().size.x, "x", get_window().size.y)
+	var resIndex = GuiAutoload.resolutions.keys().find(windows_size)
+	resolutionOptions.select(resIndex)
+
+func _on_resolution_options_item_selected(index: int):
+	var key = resolutionOptions.get_item_text(index)
+	get_window().set_size(GuiAutoload.resolutions[key])
+
+func _on_full_screen_check_box_toggled(toggled_on: bool):
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+func _on_volume_slider_value_changed(value: float):
+	if value == 0:
+		AudioServer.set_bus_mute(master_bus, true)
+	else:
+		AudioServer.set_bus_mute(master_bus, false)
+		AudioServer.set_bus_volume_db(master_bus, VOLUME_LIMIT * (1 - pow(value/100, 2)))
