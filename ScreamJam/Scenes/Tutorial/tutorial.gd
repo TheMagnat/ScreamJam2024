722799b799957e61c2a27ea81bf393cdc0e50891
ObaniGemini <robin@pi-et-ro.net>
2024-10-19 19:33:15 +0200

    Fix noclip in tutorial

--- ScreamJam/Scenes/Tutorial/tutorial.gd
+++ ScreamJam/Scenes/Tutorial/tutorial.gd
@@ -113,7 +113,7 @@ func move(a: float):
 	var dir : Vector3 = Vector3(-sin($Camera3D.rotation.y + a), 0.0, -cos($Camera3D.rotation.y + a)) * $Map.gridSpace
 	var expectedPos : Vector3 = ($Camera3D.position + dir)/$Map.gridSpace
 	
-	if !$Map.isAvailable(Vector2i(expectedPos.x + 0.1, expectedPos.z + 0.1)):
+	if !$Map.isAvailable(Vector2i(expectedPos.x + signf(expectedPos.x) * 0.1, expectedPos.z + signf(expectedPos.z) * 0.1)):
 		return
 	
 	moving = true
