ab31fa1f23e68fa88009e11ec4d58ba53b19b31d
ObaniGemini <robin@pi-et-ro.net>
2024-10-20 15:40:26 +0200

    Simple death/respawn code

--- ScreamJam/addons/fpc/character.gd
+++ ScreamJam/addons/fpc/character.gd
@@ -6,8 +6,6 @@
 
 class_name Character extends CharacterBody3D
 
-signal die
-
 ## The settings for the character's movement and feel.
 @export_category("Character")
 ## The speed that the character moves at without crouching or sprinting.
@@ -115,6 +113,8 @@ const HEALTH_RECOVER := 1.0
 var health := HEALTH_MAX
 
 func damageSanity(dmg: float, eyes_closed_factor := 1.0, under_zero_factor := 1.0):
+	if dead: return
+	
 	var oldSanityZ: float = minf(0.0, sanity)
 	sanity -= dmg * (eyes_closed_factor if closed_eyes else 1.0)
 	
@@ -130,6 +130,8 @@ var damageTween: Tween
 var lastTweenDelta: float = 0.0
 var lastRotDiff: Vector3
 func damageHealth(dmg: float, dot := false):
+	if dead: return
+	
 	health -= dmg
 	if !dot:
 		if damageTween:
@@ -157,7 +159,39 @@ func damageHealth(dmg: float, dot := false):
 		0.0, 1.0, 0.3)
 	
 	if health < 0.0:
-		die.emit()
+		die()
+
+var dead := false
+func die():
+	dead = true
+	set_physics_process(false)
+	set_process_input(false)
+	blink(true)
+	$GridRestrictor.deactivate()
+	
+	await get_tree().create_timer(2.0).timeout
+	spawn()
+
+func spawn():
+	position = map.playerSpawn
+	sanity = SANITY_MAX
+	health = HEALTH_MAX
+	
+	HEADBOB_ANIMATION.play("RESET")
+	JUMP_ANIMATION.play("RESET")
+	CROUCH_ANIMATION.play("RESET")
+	
+	enter_normal_state()
+	
+	set_physics_process(true)
+	set_process_input(true)
+	
+	$PostProcess/ColorRect.material.set_shader_parameter("blink", 1.0)
+	blink(true)
+	
+	$GridRestrictor.activate()
+	
+	dead = false
 
 func _ready():
 	Global.player = self
@@ -175,22 +209,9 @@ func _ready():
 	
 	# Reset the camera position
 	# If you want to change the default head height, change these animations.
-	HEADBOB_ANIMATION.play("RESET")
-	JUMP_ANIMATION.play("RESET")
-	CROUCH_ANIMATION.play("RESET")
-	
-	enter_normal_state()
-	
 	check_controls()
 	
-	$PostProcess/ColorRect.material.set_shader_parameter("blink", 1.0)
-	blink(true)
-	await get_tree().create_timer(0.5).timeout
-	if not Debug.debug:
-		global_position = map.playerSpawn
-	blink(false)
-	
-	$GridRestrictor.activate()
+	spawn()
 
 func check_controls(): # If you add a control, you might want to add a check for it here.
 	# The actions are being disabled so the engine doesn't halt the entire project in debug mode
