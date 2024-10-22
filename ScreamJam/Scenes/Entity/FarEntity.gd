0fefb9ca0c7e5ebbd7474dd904c219d17d9e1fda
buloz <lbizeray30@gmail.com>
2024-10-22 23:01:42 +0200

    far entities lifespan

--- ScreamJam/Scenes/Entity/FarEntity.gd
+++ ScreamJam/Scenes/Entity/FarEntity.gd
@@ -2,7 +2,11 @@ extends Node3D
 
 
 @export var minDistance: float = 8.5
+@export var lifespan: float = 12.0
 @onready var material: ShaderMaterial = $Sprite3D.material_override
+
+func _ready():
+	get_tree().create_timer(lifespan).timeout.connect(queue_free)
 
 func _process(_delta: float) -> void:
 	var distToPlayer: float = global_position.distance_to(Global.player.global_position)
