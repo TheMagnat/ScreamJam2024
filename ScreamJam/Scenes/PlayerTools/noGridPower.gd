298d2101a70b2856e9c233f09cabd682a958b633
obani <robin@pi-et-ro.net>
2024-10-23 21:32:53 +0200

    nogridpower sound

--- ScreamJam/Scenes/PlayerTools/noGridPower.gd
+++ ScreamJam/Scenes/PlayerTools/noGridPower.gd
@@ -10,6 +10,9 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 	var tween: Tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
 	tween.tween_property(material, "shader_parameter/fadeOut", 1.0, 1.5)
 	tween.parallel().tween_property(self, "scale", Vector3.ONE * 2.0, 1.5)
-	tween.parallel().tween_property(Global.player.get_node("PostProcess/ColorRect").material, "shader_parameter/tookPower", 1.0, 3.0)
-	tween.tween_property(Global.player.get_node("PostProcess/ColorRect").material, "shader_parameter/tookPower", 0.0, 0.5)
+	tween.parallel().tween_property(Global.player.get_node("PostProcess/ColorRect").material, "shader_parameter/tookPower", 1.0, 5.0)
+	tween.tween_property(Global.player.get_node("PostProcess/ColorRect").material, "shader_parameter/tookPower", 0.0, 2.0)
+	tween.tween_interval(6.0)
 	tween.tween_callback(queue_free)
+	
+	$AudioStreamPlayer.play()
