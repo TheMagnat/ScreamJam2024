4276bc02206ae9689e23ed1b7884bd2176180ec2
obani <robin@pi-et-ro.net>
2024-10-23 20:16:04 +0200

    death and hit sounds for skeletons

--- ScreamJam/Scenes/Enemies/GridMobModel.gd
+++ ScreamJam/Scenes/Enemies/GridMobModel.gd
@@ -12,6 +12,7 @@ func takeHit():
 	
 	dmgTween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
 	dmgTween.tween_property(material_override, "shader_parameter/hit", 0.0, 0.5)
+	$takeHit.play()
 	#dmgTween.parallel().tween_property(self, "scale", Vector3.ONE * 1.25, 0.05)
 	#dmgTween.tween_property(self, "scale", Vector3.ONE, 0.25)
 	
@@ -20,6 +21,6 @@ func die():
 	material_override.set_shader_parameter("death", 0.0)
 	dmgTween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
 	dmgTween.tween_property(material_override, "shader_parameter/death", 1.0, 1.0)
+	dmgTween.tween_interval(1.0)
 	dmgTween.tween_callback(get_parent().queue_free)
-	
-	
+	$die.play()
